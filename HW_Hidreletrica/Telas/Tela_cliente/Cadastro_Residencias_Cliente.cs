0b5a2ce23a3331a85f1befbe3e2d3edc2c0aefd4
Luis_Felipe <78570260+Souzlipes@users.noreply.github.com>
2023-05-30 22:13:33 -0300

    Estrutura Sistema HW Hidreletrica

---
+++ HW_Hidreletrica/Telas/Tela_cliente/Cadastro_Residencias_Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW_Hidreletrica.Telas.Tela_cliente
+{
+    public partial class Cadastro_Residencias_Cliente : Form
+    {
+        public Cadastro_Residencias_Cliente()
+        {
+            InitializeComponent();
+        }
+    }
+}
