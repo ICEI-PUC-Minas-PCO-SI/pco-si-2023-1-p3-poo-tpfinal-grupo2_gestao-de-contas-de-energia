cb03959531611b4c6c004b02d458f2fdc1ba1030
Luis_Felipe <78570260+Souzlipes@users.noreply.github.com>
2023-06-01 21:02:54 -0300

    Tela principal + Login

--- HW_Hidreletrica/Telas/Login.Designer.cs
+++ HW_Hidreletrica/Telas/Login.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Login";
+            SuspendLayout();
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.DarkSlateGray;
+            ClientSize = new Size(390, 444);
+            Name = "Login";
+            Text = "Login";
+            ResumeLayout(false);
         }
 
         #endregion
--- HW_Hidreletrica/Telas/Login.cs
+++ HW_Hidreletrica/Telas/Login.cs
@@ -16,5 +16,6 @@ namespace HW_Hidreletrica.Telas
         {
             InitializeComponent();
         }
+
     }
 }
