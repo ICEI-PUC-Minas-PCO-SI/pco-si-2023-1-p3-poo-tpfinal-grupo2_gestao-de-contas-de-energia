6b8a55fd7fe151283e06bd25b91214a756c44b4f
Gustavo Alves Teixeira <1350152@sga.pucminas.br>
2023-06-07 21:23:33 -0300

    Alterações de propriedades na Tela de Perfil

--- HW_Hidreletrica/Telas/Tela_cliente/Perfil_Cliente.cs
+++ HW_Hidreletrica/Telas/Tela_cliente/Perfil_Cliente.cs
@@ -26,5 +26,25 @@ namespace HW_Hidreletrica.Telas.Tela_cliente
         {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
