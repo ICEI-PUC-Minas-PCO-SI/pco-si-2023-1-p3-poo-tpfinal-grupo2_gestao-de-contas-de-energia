347d68f92cd2ffa48095f1650134b51ad5daa0b6
LucasFerreiraGuedes <lucasguedes01673874@gmail.com>
2023-06-09 23:14:00 -0300

    resolvendo conflitos de arquivos de solução e versionamento

--- HW_Hidreletrica/Program.cs
+++ HW_Hidreletrica/Program.cs
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-using HW_Hidreletrica.Entidades.Usuario.Cliente.Conta;
-=======
->>>>>>> Gustavo-Alves
-using HW_Hidreletrica.Telas.Tela_cliente;
 
+using HW_Hidreletrica.Telas.Tela_cliente;
 namespace HW_Hidreletrica
 {
     internal static class Program
@@ -17,11 +13,8 @@ namespace HW_Hidreletrica
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-<<<<<<< HEAD
-            Application.Run(new Contas_Clientes());
-=======
             Application.Run(new Cadastro_Cliente());
->>>>>>> Gustavo-Alves
+
         }
     }
 }
